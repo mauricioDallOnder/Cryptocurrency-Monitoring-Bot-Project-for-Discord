b1d1bfdd4d0e7ef73d12d380b0af5f1d579dca1f
Mauricio Dall Onder <londer11@icloud.com>
2023-07-18 21:15:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits,ActivityType } = require('discord.js');
 require('dotenv').config()
 const axios = require('axios');
